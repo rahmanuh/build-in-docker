27042bf69f3d9509b951a40752da2fca7a60da48
Rahmanu Hermawan <rahmanu@pasdirumah.com>
2022-12-29 15:15:23 +0100

    Separate yocto and android build image

---
+++ yocto_env.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Here are some default settings.
+# Make sure DOCKER_WORKDIR is created and owned by current user.
+
+# Docker
+
+DOCKER_IMAGE_TAG="yocto-build"
+DOCKER_WORKDIR="/home/rahmanu/bind_ext_hdd/build-docker-workdir"
